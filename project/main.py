c65e4511486da26a4905aede6706a943ebe709ce
Lucas Rodriguez Sinni <sinniknot@gmail.com>
2025-03-16 22:59:31 -0300

    Graphic Interface added
    Using PyWebIO

--- project/main.py
+++ project/main.py
@@ -1,30 +1,55 @@
-import scryfall_api  # Módulo para interagir com a API Scryfall
-import mana_analysis  # Módulo para análise de mana do deck
-import os  # Módulo para comandos do sistema operacional
+from pywebio import start_server
+from pywebio.input import input, TEXT
+from pywebio.output import put_text, put_table, put_buttons, put_html, clear
+import scryfall_api
+import mana_analysis
 
-# Limpa o terminal sempre que o código for executado (para melhor visualização durante testes)
-os.system("cls")
+deck = {}
 
-insert_cards = True  # Variável de controle para inserção de cartas
+def add_card():
+    while True:
+        card_name = input(
+        "Digite o nome da carta (ou clique em 'EXIT' para finalizar):",
+        type="text")
+        
+        if card_name.lower() == "exit":
+            break
+        
+        card_info = scryfall_api.get_card_info(card_name)
+        
+        if card_info:
+            deck[str(card_info["name"]).lower()] = card_info
+            put_text(f"Adicionada: {card_info['name']} - {card_info['mana_cost']}")
+            put_html(f"<img src =\"{card_info['image_uris']['small']}\" alt=\"card_image\" style=\"max-width:80%;\">")
 
-deck = {}  # Dicionário que armazenará as cartas do deck
+            
+        else:
+            put_text("Carta não encontrada. Tente novamente.")
 
-# Loop para permitir a inserção contínua de cartas
-while insert_cards:
-    card_name = scryfall_api.set_card_name()  # Obtém o nome da carta digitado pelo usuário
+def analyze_deck():
+    if not deck:
+        put_text("Nenhuma carta adicionada ao deck ainda.")
+        return
     
-    if card_name.lower() == "exit":  # Se o usuário digitar "exit", o loop é interrompido
-        break
+    clear()
     
-    card_info = scryfall_api.get_card_info(card_name)  # Busca informações da carta na API
+    mana_analysis.color_count(deck)
+    mana_analysis.average_mana(deck)
+    mana_analysis.average_cmc(deck)
     
-    if card_info:  # Se a carta for encontrada, exibe suas informações
-        scryfall_api.show_card_info(card_info)
-    
-    if card_info is not None:  # Se a carta for válida, adiciona ao deck
-        deck[str(card_info["name"]).lower()] = card_info
-    
-# Realiza a contagem de cores de mana no deck inserido
-mana_analysis.color_count(deck)
-mana_analysis.average_mana(deck)
-mana_analysis.average_cmc(deck)
+    put_table([
+        ["Cor", "Quantidade"],
+        ["Vermelho (R)", mana_analysis.red_mana],
+        ["Azul (U)", mana_analysis.blue_mana],
+        ["Verde (G)", mana_analysis.green_mana],
+        ["Branco (W)", mana_analysis.white_mana],
+        ["Preto (B)", mana_analysis.black_mana],
+        ["CMC Médio", f"{mana_analysis.cmc / len(deck):.2f}"],
+    ])
+
+def main():
+    put_text("Bem-vindo ao analisador de deck de Magic: The Gathering!")
+    put_buttons(["Adicionar Cartas", "Analisar Deck"], [add_card, analyze_deck])
+
+if __name__ == "__main__":
+    start_server(main, port=8080, debug=True)
